54f7f2f7712064af6631cdd825aafe8619ec22a1
Jon R. Roma <jonroma@jonroma.net>
2020-06-16 15:51:50 -0500

    Add cidr_blocks_extra parameter

--- server.tf
+++ server.tf
@@ -1,3 +1,7 @@
+locals {
+  description = (length(var.cidr_blocks_extra) > 0) ? "Allow inbound traffic from ${local.client_sg_name} security group and extra CIDR blocks" : "Allow inbound traffic from ${local.client_sg_name} security group"
+}
+
 resource "aws_security_group_rule" "server-ingress" {
   source_security_group_id = aws_security_group.client.id
   type                     = "ingress"
@@ -7,9 +11,20 @@ resource "aws_security_group_rule" "server-ingress" {
   security_group_id        = aws_security_group.server.id
 }
 
+resource "aws_security_group_rule" "server-ingress-extra" {
+  count = length(var.cidr_blocks_extra) > 0 ? 1 : 0
+
+  cidr_blocks       = var.cidr_blocks_extra
+  type              = "ingress"
+  from_port         = var.port
+  to_port           = var.port
+  protocol          = "tcp"
+  security_group_id = aws_security_group.server.id
+}
+
 resource "aws_security_group" "server" {
   name        = local.server_sg_name
-  description = "Allow inbound traffic from ${local.client_sg_name} security group"
+  description = local.description
   vpc_id      = data.aws_vpc.selected.id
   tags        = merge(var.tags, { "Name" = local.server_sg_name })
 
