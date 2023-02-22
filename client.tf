00b9df17e0af0a6d51041fd581001fc5f80b2680
Jon R. Roma <roma@illinois.edu>
2023-02-21 18:07:28 -0600

    Allow multi-port, multi-protocol security groups
    Support security groups that allow arbitrarily many
    protocols and ports. The new `rules` argument acts
    like a map. Each optional key represents one of the
    supported protocols (ICMP, TCP, and UDP).
    
    The value stored under each map key is a list of ports
    to be granted access through the security group created
    as a result.
    
    For the ICMP protocol, use one or more ICMP type number(s)
    in the port list.

--- client.tf
+++ client.tf
@@ -1,17 +1,21 @@
 resource "aws_security_group_rule" "client-egress" {
   source_security_group_id = aws_security_group.server.id
   type                     = "egress"
-  from_port                = var.port
-  to_port                  = var.port
-  protocol                 = var.protocol
   security_group_id        = aws_security_group.client.id
+
+  # Iterate over each rule.
+  for_each = local.rule_map
+
+  from_port = each.value.port
+  to_port   = each.value.port
+  protocol  = each.value.protocol
 }
 
 resource "aws_security_group" "client" {
   name        = local.client_sg_name
   description = "Allow outbound traffic to ${local.server_sg_name} security group"
   vpc_id      = data.aws_vpc.selected.id
-  tags        = merge(var.tags, { "Name" = local.client_sg_name })
+  tags        = merge(var.tags, { Name = local.client_sg_name })
 
   lifecycle {
     ignore_changes = [description]
